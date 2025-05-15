b5f3803e72fabf64deab053185a0ffd84de7b730
holyhloy <tlegchilin@yandex.ru>
2025-05-15 12:17:33 +0300

    feat: created get_session generator for session yielding

--- src/db/session.py
+++ src/db/session.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator, Any
+
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 
 from src.core.config import settings
@@ -5,3 +7,12 @@ from src.core.config import settings
 
 engine = create_async_engine(settings.DB_URL, pool_pre_ping=True)
 Session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def get_session() -> AsyncGenerator[AsyncSession, Any]:
+    """
+    Yields session for manipulating with database data
+
+    :return: session object
+    """
+    async with Session() as session:
+        yield session
