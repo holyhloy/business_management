7c3e9a092c73b110e400d0fdd23eb7bbc985d40c
holyhloy <tlegchilin@yandex.ru>
2025-05-16 09:22:54 +0300

    feat: added enums for evaluation model and schema (score)

--- src/models/evaluation.py
+++ src/models/evaluation.py
@@ -1,9 +1,18 @@
+import enum
 import uuid
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import Enum, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import Base
+
+
+class ScoreEnum(int, enum.Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
 
 
 class Evaluation(Base):
@@ -12,7 +21,7 @@ class Evaluation(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id"), unique=True)
     user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"))
-    score: Mapped[int] = mapped_column()
+    score: Mapped[ScoreEnum] = mapped_column(Enum(ScoreEnum))
 
     task: Mapped["Task"] = relationship(back_populates="evaluation")
     user: Mapped["User"] = relationship(back_populates="evaluations")
--- src/schemas/evaluation.py
+++ src/schemas/evaluation.py
@@ -1,19 +1,28 @@
+import enum
 from uuid import UUID
 
 from pydantic import BaseModel
 
 
+class ScoreEnum(int, enum.Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+
+
 class EvaluationCreateSchema(BaseModel):
     task_id: int
     user_id: UUID
-    score: int
+    score: ScoreEnum
 
 
 class EvaluationReadSchema(BaseModel):
     id: int
     task_id: int
     user_id: UUID
-    score: int
+    score: ScoreEnum
 
     class Config:
         orm_mode = True
