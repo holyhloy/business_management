4d4dc40be1698516e195eecb7e93dbeb7248c1e8
holyhloy <tlegchilin@yandex.ru>
2025-05-16 17:11:39 +0300

    feat: added database session dependency

---
+++ src/api/deps.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+
+from fastapi.params import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.db.session import get_session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
