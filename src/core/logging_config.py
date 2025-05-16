4433ecd1f46ca8fb61fd15e60c2c582ddd3809fe
holyhloy <tlegchilin@yandex.ru>
2025-05-16 11:57:30 +0300

    feat: logging configuruation has been set up

---
+++ src/core/logging_config.py
@@ -0,0 +1,48 @@
+import logging
+import sys
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "()": "uvicorn.logging.DefaultFormatter",
+            "fmt": "%(levelprefix)s | %(asctime)s | %(name)s | %(message)s",
+            "use_colors": True,
+        },
+        "verbose": {
+            "format": "%(levelname)s | %(asctime)s | %(name)s | %(filename)s:%(lineno)d | %(message)s"
+        },
+    },
+    "handlers": {
+        "default": {
+            "level": "INFO",
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+        },
+        "file": {
+            "level": "DEBUG",
+            "formatter": "verbose",
+            "class": "logging.FileHandler",
+            "filename": "logs/app.log",
+        },
+    },
+    "loggers": {
+        "uvicorn.error": {
+            "level": "INFO",
+            "handlers": ["default", "file"],
+            "propagate": False,
+        },
+        "uvicorn.access": {
+            "level": "INFO",
+            "handlers": ["default"],
+            "propagate": False,
+        },
+        "app": {
+            "level": "DEBUG",
+            "handlers": ["default", "file"],
+            "propagate": False,
+        },
+    },
+}
