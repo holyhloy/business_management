c784bb3cf99eae09af41508686b98950489210c6
holyhloy <tlegchilin@yandex.ru>
2025-05-16 16:45:36 +0300

    fix: edited User model - added email field, widen first name and last name fields

--- migrations/versions/79b5d8a2f07b_added_name_fields_to_users_table.py
+++ migrations/versions/79b5d8a2f07b_added_name_fields_to_users_table.py
@@ -7,9 +7,8 @@ Create Date: 2025-05-16 12:10:50.398424
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '79b5d8a2f07b'
--- src/models/user.py
+++ src/models/user.py
@@ -1,20 +1,28 @@
+import enum
 import uuid
 from typing import List, Optional
 
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.models.base import Base
 
 
+class RoleEnum(str, enum.Enum):
+    EMPLOYEE = "employee"
+    MANAGER = "manager"
+    ADMIN = "admin"
+
+
 class User(SQLAlchemyBaseUserTableUUID, Base):
-    first_name: Mapped[str]
-    last_name: Mapped[str]
+    first_name: Mapped[str] = mapped_column(String(64), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(128), nullable=True)
     team_id: Mapped[Optional[int]] = mapped_column(
         ForeignKey("teams.id"), nullable=True
     )
-    role: Mapped[str] = mapped_column(default="employee")
+    role: Mapped[RoleEnum] = mapped_column(default=RoleEnum.EMPLOYEE)
+    email: Mapped[str] = mapped_column(String(100), nullable=False)
 
     team: Mapped[Optional["Team"]] = relationship(back_populates="users")
     tasks: Mapped[List["Task"]] = relationship(back_populates="assignee")
