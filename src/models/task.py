0e3d16e939a1893fbc5ddfc4ade3ca674a49d547
holyhloy <tlegchilin@yandex.ru>
2025-05-16 09:49:39 +0300

    feat: created first migration, all tables were created

--- src/models/task.py
+++ src/models/task.py
@@ -46,7 +46,7 @@ class TaskComment(Base):
     task_id: Mapped[int] = mapped_column(ForeignKey("tasks.id"))
     user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("user.id"))
     content: Mapped[str] = mapped_column(Text)
-    created_at: Mapped[datetime] = mapped_column(default=datetime.now(datetime.UTC))
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
 
     task: Mapped["Task"] = relationship(back_populates="comments")
     user: Mapped["User"] = relationship()
