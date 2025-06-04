419042b88a34c9025eab98af2c5063dd2c53448d
holyhloy <tlegchilin@yandex.ru>
2025-06-04 12:30:33 +0300

    feat: added authorization checking on frontend root endpoint to avoid excessive redirects

--- src/frontend/root.py
+++ src/frontend/root.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi.responses import RedirectResponse
+
+from src.auth.auth import current_user_optional
+from src.models import User
 
 router = APIRouter()
 
 
 @router.get("/")
-async def root():
-    return RedirectResponse(url="/auth")
+async def root(user: User = Depends(current_user_optional)):
+    if not user:
+        return RedirectResponse(url="/auth")
+    return RedirectResponse(url="/index")
