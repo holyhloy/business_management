14135314e10c6d485b988dfa09e56b09d2dd7577
holyhloy <tlegchilin@yandex.ru>
2025-06-04 10:40:59 +0300

    feat: added get_user_manager generator instead of directly UserManager calling

--- src/middleware/auth_context.py
+++ src/middleware/auth_context.py
@@ -4,7 +4,7 @@ from fastapi import Request
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from src.auth.auth import get_jwt_strategy
-from src.auth.manager import UserManager, get_user_db
+from src.auth.manager import get_user_db, get_user_manager
 from src.core.logging_config import logger
 
 
@@ -21,7 +21,8 @@ class InjectUserMiddleware(BaseHTTPMiddleware):
 
             user_db_gen = get_user_db()
             user_db = await anext(user_db_gen)
-            user_manager = UserManager(user_db)
+            user_manager_gen = get_user_manager(user_db)
+            user_manager = await anext(user_manager_gen)
             user = await self.strategy.read_token(token, user_manager)
         except Exception as e:
             logger.info(f"InjectUserMiddleware: Unable to load user - {e}")
