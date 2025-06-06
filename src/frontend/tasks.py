257894b7419c301e49dba4983eecde7f7128e0d2
holyhloy <tlegchilin@yandex.ru>
2025-06-06 10:10:15 +0300

    ref: renamed user_list and tasks_list vars

--- src/frontend/tasks.py
+++ src/frontend/tasks.py
@@ -2,9 +2,9 @@ from fastapi import APIRouter, Depends
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 
-from src.auth.auth import current_user_optional
 from src.dependencies.deps import SessionDep
 from src.frontend.config import render_template
+from src.auth.auth import current_user_optional
 from src.models import User
 from src.services.task_service import list_tasks
 from src.services.user_service import get_all_users
@@ -18,8 +18,8 @@ async def get_frontend_my_tasks(
 ):
     if not user:
         return RedirectResponse(url="/auth", status_code=302)
-    user_list = await get_all_users(session)
-    tasks_list = await list_tasks(session, user.id)
+    all_users = await get_all_users(session)
+    user_tasks_list = await list_tasks(session, user.id)
     return render_template(
-        "tasks.html", request, {"users": user_list, "tasks": tasks_list}
+        "tasks.html", request, {"users": all_users, "tasks": user_tasks_list}
     )
